aa8af0a298a0ae541183224da6979162997a5769
Seizerg <63910941+Seizerg@users.noreply.github.com>
2021-08-30 07:31:56 +0530

    keeper-app

---
+++ build/precache-manifest.91c531240cc65194c2e315037501fbb9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d88cf8740dcc90246df9fa60420818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c24a7194b105e8964fe5",
+    "url": "/static/js/2.c12cb578.chunk.js"
+  },
+  {
+    "revision": "049cc997702a24494bdc",
+    "url": "/static/js/main.1cb3ca27.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
